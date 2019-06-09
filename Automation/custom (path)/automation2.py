4461ac8ba21796806a271cc90fe86b018d546d3f
Andreas <Andreasgdp@gmail.com>
2019-06-09 13:46:52 +0200

    Fixed the automation of DCP from terminal

--- Automation/custom (path)/automation2.py
+++ Automation/custom (path)/automation2.py
@@ -1,14 +1,14 @@
 import sys
 import os
 
-# path = '../{}/'.format(str(sys.argv[1]))
+# path = '../../{}/'.format(str(sys.argv[1]))
 
-assignment = '../{}/{}'.format(str(sys.argv[1]), 'ASSIGNMENT.md')
+assignment = '../../{}/{}'.format(str(sys.argv[1]), 'ASSIGNMENT.md')
 f1 = open(assignment, 'a')
 f1.write('# Assignment ({})'.format(str(sys.argv[1])))
 f1.close()
 
-notes = '../{}/{}'.format(str(sys.argv[1]), 'NOTES.md')
+notes = '../../{}/{}'.format(str(sys.argv[1]), 'NOTES.md')
 f2 = open(notes, 'a')
 f2.write('# Notes ({})'.format(str(sys.argv[1])))
 f2.close()
